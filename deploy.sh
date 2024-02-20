f261a82aea356fcbf016944dbc1b42fce85cc3c9
sunlulu.tomato <137380027@qq.com>
2024-02-20 15:17:11 +0800

    add deploy shell script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+set -ex
 
+# Clean first
+./gradlew clean
+
+# Get remote info
+git_remote_info=$(git remote -v | grep origin | grep fetch | awk '{print $2}')
+git_commit=$(git rev-parse --short HEAD)
 # Generate Dokka documentation
 ./gradlew gitlab:dokkaHtml
 
 # Move to the directory where the dokka html is generated
 # shellcheck disable=SC2164
-cd build/dokka/html
+cd gitlab/build/dokka/html
 
 # Initialize a new git repository
 git init
@@ -14,8 +21,8 @@ git init
 git add .
 
 # Commit the changes
-git commit -a -m "Deploy dokka html to github pages"
+git commit -a -m "Deploy dokka html to github pages @${git_commit}"
 
 # Push the changes to the pages branch of your repository
-git_remote_info=$(git remote -v | grep origin | grep fetch | awk '{print $2}')
-git push -f "${git_remote_info}" origin pages
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+git push -f "${git_remote_info}" "${git_branch}:pages"
