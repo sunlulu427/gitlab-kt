6dc9ea54d15a6cbf1bafcc7879772a62612e78e5
sunlulu.tomato <137380027@qq.com>
2024-02-21 23:23:08 +0800

    add publish script for github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+
 plugins {
     kotlin("jvm") version "1.9.21"
     kotlin("plugin.serialization") version "1.9.21"
     id("org.jetbrains.dokka") version "1.9.10"
+    id("maven-publish")
 }
 
-group = "com.mato.gitlab"
-version = "1.0-SNAPSHOT"
+group = "com.mato"
+archivesName = "gitlab"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -21,4 +25,33 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+subprojects {
+    apply(plugin = "maven-publish")
+    apply(plugin = "java")
+    System.getenv()["GITHUB_TOKEN"]?.let {
+        configure<PublishingExtension> {
+            repositories {
+                maven {
+                    name = "GitHubPackages"
+                    url = uri("https://maven.pkg.github.com/sunlulu427/gitlab-kt")
+                    credentials {
+                        username = System.getenv("GITHUB_USERNAME")
+                        password = it
+                    }
+                }
+            }
+            publications {
+                register<MavenPublication>("jar") {
+                    groupId = "com.mato"
+                    artifactId = "gitlab"
+
+                    afterEvaluate {
+                        from(components["java"])
+                    }
+                }
+            }
+        }
+    }
 }
